ba9943e77640b3ea3fd758b2c2ef914c2531d3e2
NelsonWang <wangnelson2@gmail.com>
2023-09-13 21:45:59 -0300

    clase 2 codigo

---
+++ clase-02/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { AppPageWrapperComponent } from './components/app-page-wrapper/app-page-wrapper.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ToolbarComponent,
+    SidebarComponent,
+    AppPageWrapperComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ clase-02/src/app/components/app-page-wrapper/app-page-wrapper.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-wrapper',
+  templateUrl: './app-page-wrapper.component.html',
+  styleUrls: ['./app-page-wrapper.component.scss']
+})
+export class AppPageWrapperComponent {
+  
+}
